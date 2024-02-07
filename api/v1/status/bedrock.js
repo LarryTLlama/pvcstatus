c8142e61ee2ae6b171472df896493a84b1f1afc7
ThatAdamM <larrytllama5@gmail.com>
2024-02-07 23:03:56 +0000

    v1.0.1 - Minor edits to fail text

--- api/v1/status/bedrock.js
+++ api/v1/status/bedrock.js
@@ -8,6 +8,6 @@ export default async function handler(req, res) {
         return res.json({ color: "green", btncolor: "success", online: true, text: "Bedrock is online!", additionalInfo: {responsetime: (status.retrieved_at - reqtime), version: status.version.name, players: status.players.online, motd: status.motd.html}});
     })
     .catch((error) => {
-        return res.json({ color: "red", btncolor: "danger", online: false, text: "Unable to contact status checker server!"})
+        return res.json({ color: "red", btncolor: "warning", online: false, text: "Might not be offline! Unable to contact status checker server!"})
     })
   }
